584b9196f186e3a5a85c97e22c22c180f5913153
Nish1423 <115086590+Nish1423@users.noreply.github.com>
2025-08-31 12:40:35 +0900

    Update vite.config.ts

--- brew-journal-world-map/vite.config.ts
+++ brew-journal-world-map/vite.config.ts
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '',
+  base: '/brew-journal-world-map/',  // Adjusted for your GitHub Pages folder
 })
